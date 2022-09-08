fbd26c9d59d526a323bdf6309ea69f6ddce0904a
The1Simplified <scheiterlem@gmail.com>
2022-09-08 11:12:37 -0400

    Added profile icon to Sidebar & cleanup

--- dev.py
+++
@@ -1,9 +0,0 @@
-"""
-Used exclusivly for one off scripts to setup the dev environment
-
-Run this file to create and setup dev db before running the website
-"""
-
-from config import create_dev_tables
-
-create_dev_tables()
